ab8220c45b2c184d04fd3ad5c51e4a17e883bb57
nase00 <nase00@gmail.com>
2015-04-06 01:49:52 -0500

    Update tests. Still WIP.

--- test/test.js
+++ test/test.js
@@ -2,9 +2,10 @@ var assert = require("assert"),
 		module = require('../index.js')
 
 describe('react-shift', function(){
+	var Shift = require('../index.js')
   describe('paginationArray', function(){
     it('should return an array containing a range of numbers', function(){
-      assert.equal(paginationArray, 0);
+      assert.equal(Shift, 0);
     })
   })
 })
