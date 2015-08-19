721b6116e5a877d98658a8055a3ea7e85692e0dc
Sean Owiecki <sowiecki@walmartlabs.com>
2015-08-18 23:27:48 -0500

    Enforce semi-colon eslint rule, fix lint errors.

--- src/handle-swipe.js
+++ src/handle-swipe.js
@@ -1,31 +1,29 @@
-var position = {
-	right: 0
-	, left: 0
-	, direction: null
-	, clear: function() {
-		this.right = 0
-		this.left = 0
+const position = {
+	right: 0,
+	left: 0,
+	direction: null,
+	clear: () => {
+		this.right = 0;
+		this.left = 0;
 	}
-}
+};
 
-var handleSwipe = function(e) {
-	e = Math.round(e)
-	position.direction = null
+export default handleSwipe = (e) => {
+	e = Math.round(e);
+	position.direction = null;
 	if (e < 450) {
-		position.left += 1
-		position.right = 0
+		position.left += 1;
+		position.right = 0;
 	} else if (e > 550) {
-		position.right += 1
-		position.left = 0
+		position.right += 1;
+		position.left = 0;
 	}
 	if (position.left > 4) {
-		position.clear()
-		return 'left'
+		position.clear();
+		return 'left';
 	}
 	if (position.right > 4) {
-		position.clear()
-		return 'right'
+		position.clear();
+		return 'right';
 	}
-}
-
-module.exports = handleSwipe
+};
